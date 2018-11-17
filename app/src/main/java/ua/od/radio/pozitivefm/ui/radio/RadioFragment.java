b5db4afc482d3e86a56a055087c90b2ebff302e7
Zakharov Vladymyr <twins88odessa@gmail.com>
2018-11-17 16:49:10 +0200

    work with RadioFragment

--- app/src/main/java/ua/od/radio/pozitivefm/ui/radio/RadioFragment.java
+++ app/src/main/java/ua/od/radio/pozitivefm/ui/radio/RadioFragment.java
@@ -23,8 +23,10 @@ public class RadioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_radio, container, false);
+        View view = inflater.inflate(R.layout.fragment_radio, container, false);
+
+
+        return view;
     }
 
 }
