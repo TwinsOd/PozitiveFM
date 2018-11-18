9802871adbfb4f83656a8e2b9e996b046e9cf81b
Zakharov Vladymyr <twins88odessa@gmail.com>
2018-11-18 17:59:19 +0200

    fix notification builder

--- app/src/main/java/ua/od/radio/pozitivefm/ui/about_us/AboutUsFragment.java
+++ app/src/main/java/ua/od/radio/pozitivefm/ui/about_us/AboutUsFragment.java
@@ -23,8 +23,9 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        return view;
     }
 
 }
