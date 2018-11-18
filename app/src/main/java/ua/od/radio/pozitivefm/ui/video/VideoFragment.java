d58e8f8b87f629e7ac69c3496be4f0fc3b195b3f
Zakharov Vladymyr <twins88odessa@gmail.com>
2018-11-18 18:17:02 +0200

    work with VideoFragment

--- app/src/main/java/ua/od/radio/pozitivefm/ui/video/VideoFragment.java
+++ app/src/main/java/ua/od/radio/pozitivefm/ui/video/VideoFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import ua.od.radio.pozitivefm.R;
 
@@ -23,8 +24,10 @@ public class VideoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video, container, false);
+        View view = inflater.inflate(R.layout.fragment_video, container, false);
+        WebView webView = view.findViewById(R.id.plaer_web_view);
+        webView.loadUrl("https://goodgame.ru/channel/PozitivFM");
+        return view;
     }
 
 }
