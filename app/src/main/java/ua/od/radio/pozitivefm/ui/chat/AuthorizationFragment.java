9461127d32d1f9f143190cf89c8d0ebcf1cad054
Zakharov Vladymyr <twins88odessa@gmail.com>
2018-12-02 20:28:36 +0200

    work with AuthorizationFragment

--- app/src/main/java/ua/od/radio/pozitivefm/ui/chat/AuthorizationFragment.java
+++ app/src/main/java/ua/od/radio/pozitivefm/ui/chat/AuthorizationFragment.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ua.od.radio.pozitivefm.R;
 
 
-public class AuthorizationFragment extends DialogFragment {
-
+public class AuthorizationFragment extends DialogFragment implements View.OnClickListener {
+    private boolean isAuth = true;
+    private TextView authorizationType;
+    private TextView registrationType;
 
     public AuthorizationFragment() {
         // Required empty public constructor
@@ -22,12 +26,46 @@ public class AuthorizationFragment extends DialogFragment {
         return fragment;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getDialog().getWindow() != null)
+            getDialog().getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_authorization, container, false);
+        View view = inflater.inflate(R.layout.fragment_authorization, container, false);
+        authorizationType = view.findViewById(R.id.authorization_type);
+        authorizationType.setOnClickListener(this);
+        registrationType = view.findViewById(R.id.registration_type);
+        registrationType.setOnClickListener(this);
+        updateType();
+        return view;
     }
 
+    private void updateType() {
+        if (isAuth) {
+            authorizationType.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+            registrationType.setTextColor(getResources().getColor(R.color.colorDark));
+        } else {
+            authorizationType.setTextColor(getResources().getColor(R.color.colorDark));
+            registrationType.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.authorization_type:
+                isAuth = true;
+                updateType();
+                break;
+            case R.id.registration_type:
+                isAuth = false;
+                updateType();
+                break;
+        }
+    }
 }
